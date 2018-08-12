75f03b461a505313a3aab19e4b857c84ff1ec9a8
UncleBen69 <40238429+UncleBen69@users.noreply.github.com>
2018-08-12 01:15:42 +0100

    3.10

--- Scripts/alerter.js
+++ Scripts/alerter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿//New Dynamic shit
 dynamicVar();
 function dynamicVar(){
@@ -20,44 +19,6 @@ function dynamicVar(){
     })
 }
 
-=======
-﻿//Live
-if (!localStorage.setItem("ice_poseidon_live", false)) localStorage.setItem("ice_poseidon_live", false);
-if (!localStorage.setItem("sam_pepper_live", false)) localStorage.setItem("sam_pepper_live", false);
-if (!localStorage.setItem("tracksuit_andy_live", false)) localStorage.setItem("tracksuit_andy_live", false);
-if (!localStorage.setItem("ebz_live", false)) localStorage.setItem("ebz_live", false);
-if (!localStorage.setItem("onlyusemeblade_live", false)) localStorage.setItem("onlyusemeblade_live", false);
-if (!localStorage.setItem("marie_live", false)) localStorage.setItem("marie_live", false);
-if (!localStorage.setItem("hypeman_vince_live", false)) localStorage.setItem("hypeman_vince_live", false);
-if (!localStorage.setItem("asian_andy_live", false)) localStorage.setItem("asian_andy_live", false);
-if (!localStorage.setItem("bjorn_live", false)) localStorage.setItem("bjorn_live", false);
-if (!localStorage.setItem("gray_live", false)) localStorage.setItem("gray_live", false);
-if (!localStorage.setItem("kiedom_live", false)) localStorage.setItem("kiedom_live", false);
-if (!localStorage.setItem("sweeterin_live", false)) localStorage.setItem("sweeterin_live", false);
-if (!localStorage.setItem("mexicanandy_live", false)) localStorage.setItem("mexicanandy_live", false);
-if (!localStorage.setItem("anything4views_live", false)) localStorage.setItem("anything4views_live", false);
-if (!localStorage.setItem("hyphonix_live", false)) localStorage.setItem("hyphonix_live", false);
-if (!localStorage.setItem("mizkif_live", false)) localStorage.setItem("mizkif_live", false);
-if (!localStorage.setItem("vexxed_live", false)) localStorage.setItem("vexxed_live", false);
-//Toggles
-if (!localStorage.ice_poseidon_enabled) localStorage.setItem("ice_poseidon_enabled", "true");
-if (!localStorage.sam_pepper_enabled) localStorage.setItem("sam_pepper_enabled", "true");
-if (!localStorage.tracksuit_andy_enabled) localStorage.setItem("tracksuit_andy_enabled", "true");
-if (!localStorage.ebz_enabled) localStorage.setItem("ebz_enabled", "true");
-if (!localStorage.onlyusemeblade_enabled) localStorage.setItem("onlyusemeblade_enabled", "true");
-if (!localStorage.marie_enabled) localStorage.setItem("marie_enabled", "true");
-if (!localStorage.hypeman_vince_enabled) localStorage.setItem("hypeman_vince_enabled", "true");
-if (!localStorage.asian_andy_enabled) localStorage.setItem("asian_andy_enabled", "true");
-if (!localStorage.bjorn_enabled) localStorage.setItem("bjorn_enabled", "true");
-if (!localStorage.gray_enabled) localStorage.setItem("gray_enabled", "true");
-if (!localStorage.kiedom_enabled) localStorage.setItem("kiedom_enabled", "true");
-if (!localStorage.sweeterin_enabled) localStorage.setItem("sweeterin_enabled", "true");
-if (!localStorage.mexicanandy_enabled) localStorage.setItem("mexicanandy_enabled", "true");
-if (!localStorage.anything4views_enabled) localStorage.setItem("anything4views_enabled", "true");
-if (!localStorage.hyphonix_enabled) localStorage.setItem("hyphonix_enabled", "true");
-if (!localStorage.mizkif_enabled) localStorage.setItem("mizkif_enabled", "true");
-if (!localStorage.vexxed_enabled) localStorage.setItem("vexxed_enabled", "true");
->>>>>>> 0d04f5896633357350b05b9561802248f5461667
 //Settings
 if (!localStorage.notification_enabled) localStorage.setItem("notification_enabled", "true");
 if (!localStorage.sound_enabled) localStorage.setItem("sound_enabled", "true");
@@ -187,7 +148,6 @@ function search() {
                 if(data.streamers[number].liveData.live === true){
                     if(localStorage.getItem(id+"_live") === "false"){
                         if(localStorage.getItem(id+"_enabled") === "true"){
-<<<<<<< HEAD
                             const time = /(..)(:..)/.exec(new Date());
                             const hour = time[1] % 12 || 12;
                             const period = time[1] < 12 ? 'AM' : 'PM';
@@ -206,8 +166,6 @@ function search() {
                                 var image = "/icon/people/default.png"
                             } */
                             var image = "/icon/people/default.png"
-=======
->>>>>>> 0d04f5896633357350b05b9561802248f5461667
                             if (localStorage.getItem("notification_enabled") === "true") {
                                 const time = /(..)(:..)/.exec(new Date());
                                 const hour = time[1] % 12 || 12;
